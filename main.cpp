031993c578b15e9489cfcfd30590905935de2e5a
martins <martincorona007@gmail.com>
2019-09-29 08:56:21 -0500

    v4.0

--- main.cpp
+++ main.cpp
@@ -10,8 +10,8 @@ char get;
 //FUNCTIONS
 bool EOFF();
 int read();
-void read1(char);
-char read2();
+
+
 bool wsp();
 bool identifier();
 void fail();
@@ -20,9 +20,11 @@ void full_back();
 void token();
 
 char w;
-char set_letter();
-char get_letter(char);
+char get_letter();
+void set_letter(char);
 char casting(int);
+bool calis();
+
 int main(){
     file_p=fopen("text.txt","r");
         do
@@ -36,41 +38,147 @@ int main(){
             if(read()=='\0'){
                 printf("end");
             }*/
-/*
+            /*wsp();
             if(identifier()){
                 printf("yes");
             }else{
                 printf("nop");
-            }
-            */
-            printf("(%c)",set_letter());
+            }*/
+            
+            printf("(%c)",get_letter());
+            if(get_letter()==' '){
+                printf("yes");
+            }/*
+            if(calis()){
+                    printf("yes");
+            }else{
+                printf("nope");
+            }*/
         } while (EOFF());
         
     return 0;
 }
 
-int read(){
-    int c;
-    signed int aux;
-    char word;
-   c=fgetc(file_p);
-    aux=c;
-   // printf("[{%i}]",c);
-   //printf("[%c][%c]",c,aux);
-    word=casting(aux);
-    get_letter(word);
-    //read1(aux);
-    return c;
+bool calis(){
+    if(get_letter()==' '){
+                
+                return true;
+    }else{
         
-        
-    
+        return false;
+    }
 }
-char get_letter(char cyk){
+int read(){
+        int c;
+        signed int aux;
+        char word;
+        c=fgetc(file_p);
+        aux=c;
+    // printf("[{%i}]",c);
+    //printf("[%c][%c]",c,aux);
+        word=casting(aux);
+        set_letter(word);  
+        sucess();
+    return c;
+}
+void set_letter(char cyk){
      w=cyk;
 }
-char set_letter(){
+char get_letter(){
     return w;
 }
+
+
+void sucess(){ q=ftell(file_p); printf("\n{Activated sucess %li}",q);}
+void fail(){fseek(file_p,q,SEEK_SET);}
+void full_back(){fseek(file_p,-1,SEEK_CUR);}
+
+bool EOFF(){
+    if(read()==EOF){  
+         //  full_back();  
+        return false;      
+    }else{
+        //    full_back();
+        return true; 
+
+    }
+
+}
+bool wsp(){
+        while(isspace(read()))
+        full_back();
+        sucess();
+        
+        return true;
+}
+bool identifier(){
+    int prior;
+    int actual=0;
+    char c;
+    while(actual!=udef){
+        prior=actual;
+        c=get_letter();
+       // printf("{id %c}",c);
+        printf("<c[%c] prior[%i] actual[%i]>",c,prior,actual);
+        /*if(c=='h'){
+            printf("1[YES]");
+        }
+        if(c==104){
+            printf("2[YES]");
+        }*/
+        if(c==' '){
+                        printf("yes ");
+                    }
+        switch(actual){
+
+            case 0:
+                
+                    if((c>='a'&& c<='z')||(c>='A'&&c<='Z')){
+                        actual=2;
+                    }else if(c=='_'){
+                        actual=1;
+                    }else{
+                        actual=udef;
+                    }
+
+                break;
+            case 1:
+                    if((c>='0'&& c<='9')||(c=='_')){
+                        actual=1;
+                    }else if((c>='a'&& c<='z')||(c>='A'&&c<='Z')){
+                        actual=2;
+                    }else{
+                        actual=udef;
+                    }
+                break;
+            case 2:
+                    if(((c>='a'&& c<='z')||(c>='A'&&c<='Z'))||((c>='0'&& c<='9'))||(c=='_')){
+                        actual=2;
+                    }else{
+                        actual=udef;
+                    }
+                    
+
+                break;
+
+        }
+    
+       
+        
+    }
+     if(prior==2){
+        full_back();
+        sucess();
+
+        return true;
+    }
+    fail();
+
+    return false;
+       
+      
+}
+
 char casting(int c){
     switch (c)
     {
@@ -162,84 +270,4 @@ char casting(int c){
 
         break;
     }
-}
-
-void sucess(){ q=ftell(file_p); printf("{Activated sucess %li}",q);}
-void fail(){fseek(file_p,q,SEEK_SET);}
-void full_back(){fseek(file_p,-1,SEEK_CUR);}
-
-bool EOFF(){
-    if(read()==EOF){  
-         //   full_back();  
-        return false;      
-    }else{
-            //full_back();
-        return true; 
-
-    }
-
-}
-bool identifier(){
-    int prior;
-    int actual=0;
-    char c;
-    while(actual!=udef){
-        prior=actual;
-      //  c=read2();
-       // printf("{id %c}",c);
-       // printf("prior[%i] actual[%i] ",prior,actual);
-        /*if(c=='h'){
-            printf("1[YES]");
-        }
-        if(c==104){
-            printf("2[YES]");
-        }*/
-        switch(actual){
-
-            case 0:
-                
-                    if((c>='a'&& c<='z')||(c>='A'&&c<='Z')){
-                        actual=2;
-                    }else if(c=='_'){
-                        actual=1;
-                    }else{
-                        actual=udef;
-                    }
-
-                break;
-            case 1:
-                    if((c>='0'&& c<='9')||(c=='_')){
-                        actual=1;
-                    }else if((c>='a'&& c<='z')||(c>='A'&&c<='Z')){
-                        actual=2;
-                    }else{
-                        actual=udef;
-                    }
-                break;
-            case 2:
-                    if(((c>='a'&& c<='z')||(c>='A'&&c<='Z'))||((c>='0'&& c<='9'))||(c=='_')){
-                        actual=2;
-                    }else{
-                        actual=udef;
-                    }
-
-                break;
-
-        }
-    
-        if(prior==2){
-        //full_back();
-        //sucess();
-
-        return true;
-    }else{
-        //fail();
-
-    return false;
-    }
-        
-    }
-    
-    
-    
 }
