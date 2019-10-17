c31cc0c4a4891b7fa90c8ca98ba662a4e959e5c2
martincorona007 <martincorona007@gmail.com>
2019-10-16 20:58:46 -0500

    update 8.2 adding reserved words

---
+++ scanner.hpp
@@ -0,0 +1,29 @@
+#ifndef SCANNER_HPP
+#define SCANNER_HPP
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+//FUNCTIONS
+bool EOFF();
+int read();
+bool wsp();
+void fail();
+void sucess();
+void full_back();
+int token();
+char get_letter();
+void set_letter(char);
+char get_letter1();
+void set_letter1(char);
+char casting(int);
+void open(const char*);
+int udfa();
+int S(int,char);
+bool reserved_words();
+int Srw(int,char);
+int length(char[]);
+int compare(char[]);
+
+#endif
+
